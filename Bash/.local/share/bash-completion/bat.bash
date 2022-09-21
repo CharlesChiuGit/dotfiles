386d2ced83894b4bf6ee93dff4c98c25bac4810d
Charles Chiu <charleschiu2012@gmail.comn>
2022-09-21 16:50:20 +0800

    add bash-completion files

---
+++ Bash/.local/share/bash-completion/bat.bash
@@ -0,0 +1,101 @@
+# shellcheck disable=SC2207
+
+# Requires https://github.com/scop/bash-completion
+
+# Macs have bash3 for which the bash-completion package doesn't include
+# _init_completion. This is a minimal version of that function.
+__bat_init_completion()
+{
+	COMPREPLY=()
+	_get_comp_words_by_ref "$@" cur prev words cword
+}
+
+_bat() {
+	local cur prev words cword split=false
+	if declare -F _init_completion >/dev/null 2>&1; then
+		_init_completion -s || return 0
+	else
+		__bat_init_completion -n "=" || return 0
+		_split_longopt && split=true
+	fi
+
+	if [[ ${words[1]-} == cache ]]; then
+		case $prev in
+		--source | --target)
+			_filedir -d
+			return 0
+			;;
+		esac
+		COMPREPLY=($(compgen -W "
+			--build --clear --source --target --blank --help
+		" -- "$cur"))
+		return 0
+	fi
+
+	case $prev in
+	-l | --language)
+		local IFS=$'\n'
+		COMPREPLY=($(compgen -W "$(
+			"$1" --list-languages | while IFS=: read -r lang _; do
+				printf "%s\n" "$lang"
+			done
+		)" -- "$cur"))
+		compopt -o filenames  # for escaping
+		return 0
+		;;
+	-H | --highlight-line | --diff-context | --tabs | --terminal-width | \
+	-m | --map-syntax | --style | --line-range | -h | --help | -V | \
+	--version | --diagnostic | --config-file | --config-dir | \
+	--cache-dir | --generate-config-file)
+		# argument required but no completion available, or option
+		# causes an exit
+		return 0
+		;;
+	--file-name)
+		_filedir
+		return 0
+		;;
+	--wrap)
+		COMPREPLY=($(compgen -W "auto never character" -- "$cur"))
+		return 0
+		;;
+	--color | --decorations | --paging)
+		COMPREPLY=($(compgen -W "auto never always" -- "$cur"))
+		return 0
+		;;
+	--italic-text)
+		COMPREPLY=($(compgen -W "always never" -- "$cur"))
+		return 0
+		;;
+	--pager)
+		COMPREPLY=($(compgen -c -- "$cur"))
+		return 0
+		;;
+	--theme)
+		local IFS=$'\n'
+		COMPREPLY=($(compgen -W "$("$1" --list-themes)" -- "$cur"))
+		compopt -o filenames  # for escaping
+		return 0
+		;;
+	esac
+
+	$split && return 0
+
+	if [[ $cur == -* ]]; then
+		COMPREPLY=($(compgen -W "
+			--show-all --plain --language --highlight-line
+			--file-name --diff --diff-context --tabs --wrap
+			--terminal-width --number --color --italic-text
+			--decorations --paging --pager --map-syntax --theme
+			--list-themes --style --line-range --list-languages
+			--help --version --force-colorization --unbuffered
+			--diagnostic --config-file --config-dir --cache-dir
+			--generate-config-file
+		" -- "$cur"))
+		return 0
+	fi
+
+	_filedir
+	((cword == 1)) && COMPREPLY+=($(compgen -W cache -- "$cur"))
+
+} && complete -F _bat bat
---
+++ Bash/.local/share/bash-completion/zoxide.bash
@@ -0,0 +1,138 @@
+_zoxide() {
+	local i cur prev opts cmds
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD - 1]}"
+	cmd=""
+	opts=""
+
+	for i in ${COMP_WORDS[@]}; do
+		case "${i}" in
+		"$1")
+			cmd="zoxide"
+			;;
+		add)
+			cmd+="__add"
+			;;
+		import)
+			cmd+="__import"
+			;;
+		init)
+			cmd+="__init"
+			;;
+		query)
+			cmd+="__query"
+			;;
+		remove)
+			cmd+="__remove"
+			;;
+		*) ;;
+
+		esac
+	done
+
+	case "${cmd}" in
+	zoxide)
+		opts="-h -V --help --version add import init query remove"
+		if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]]; then
+			COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+			return 0
+		fi
+		case "${prev}" in
+		*)
+			COMPREPLY=()
+			;;
+		esac
+		COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+		return 0
+		;;
+	zoxide__add)
+		opts="-h -V --help --version <PATHS>..."
+		if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]]; then
+			COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+			return 0
+		fi
+		case "${prev}" in
+		*)
+			COMPREPLY=()
+			;;
+		esac
+		COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+		return 0
+		;;
+	zoxide__import)
+		opts="-h -V --from --merge --help --version <PATH>"
+		if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]]; then
+			COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+			return 0
+		fi
+		case "${prev}" in
+		--from)
+			COMPREPLY=($(compgen -W "autojump z" -- "${cur}"))
+			return 0
+			;;
+		*)
+			COMPREPLY=()
+			;;
+		esac
+		COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+		return 0
+		;;
+	zoxide__init)
+		opts="-h -V --no-cmd --cmd --hook --help --version bash elvish fish nushell posix powershell xonsh zsh"
+		if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]]; then
+			COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+			return 0
+		fi
+		case "${prev}" in
+		--cmd)
+			COMPREPLY=($(compgen -f "${cur}"))
+			return 0
+			;;
+		--hook)
+			COMPREPLY=($(compgen -W "none prompt pwd" -- "${cur}"))
+			return 0
+			;;
+		*)
+			COMPREPLY=()
+			;;
+		esac
+		COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+		return 0
+		;;
+	zoxide__query)
+		opts="-i -l -s -h -V --all --interactive --list --score --exclude --help --version <KEYWORDS>..."
+		if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]]; then
+			COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+			return 0
+		fi
+		case "${prev}" in
+		--exclude)
+			COMPREPLY=($(compgen -f "${cur}"))
+			return 0
+			;;
+		*)
+			COMPREPLY=()
+			;;
+		esac
+		COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+		return 0
+		;;
+	zoxide__remove)
+		opts="-i -h -V --interactive --help --version <PATHS>..."
+		if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]]; then
+			COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+			return 0
+		fi
+		case "${prev}" in
+		*)
+			COMPREPLY=()
+			;;
+		esac
+		COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+		return 0
+		;;
+	esac
+}
+
+complete -F _zoxide -o bashdefault -o default zoxide
