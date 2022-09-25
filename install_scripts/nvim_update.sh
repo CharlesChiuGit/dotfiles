4b95d3bc47872fc0186cc76c15253b1d3d61b51c
Charles Chiu <charleschiu2012@gmail.comn>
2022-09-26 05:07:07 +0800

    clean up nvim_setup_linux.sh and add nvim_update.sh

---
+++ install_scripts/nvim_update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -i
+# INFO: https://dywang.csie.cyut.edu.tw/dywang/linuxProgram/node61.html
+set -exu
+set -o pipefail
+
+if [[ ! -d "$HOME/packages/" ]]; then
+	mkdir -p "$HOME/packages/"
+fi
+
+if [[ ! -d "$HOME/tools/" ]]; then
+	mkdir -p "$HOME/tools/"
+fi
+
+#######################################################################
+#                            Nvim update                              #
+#######################################################################
+NVIM_DIR=$HOME/tools/nvim
+NVIM_SRC_NAME=$HOME/packages/nvim-linux64.tar.gz
+NVIM_LINK="https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz"
+
+echo "Updating Nvim"
+echo "Removing old Nvim binary"
+rm "$NVIM_SRC_NAME"
+rm -r "$NVIM_DIR"
+
+if [[ ! -d "$NVIM_DIR" ]]; then
+	echo "Creating nvim directory under tools directory"
+	mkdir -p "$NVIM_DIR"
+fi
+
+if [[ ! -f $NVIM_SRC_NAME ]]; then
+	echo "Downloading Nvim"
+	wget "$NVIM_LINK" -O "$NVIM_SRC_NAME"
+fi
+echo "Extracting neovim"
+tar zxvf "$NVIM_SRC_NAME" --strip-components 1 -C "$NVIM_DIR"
+
+if [[ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]]; then
+	echo "Installing packer.nvim"
+	git clone --depth=1 https://github.com/wbthomason/packer.nvim \
+		~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
+
+echo "Updating nvim plugins, please wait"
+"$NVIM_DIR/bin/nvim" -c "autocmd User PackerComplete quitall" -c "PackerSync"
+
+echo "Finished updating Nvim and its dependencies!"
