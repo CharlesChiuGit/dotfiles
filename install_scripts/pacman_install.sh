7c1472f9bd8226e29adef6d8c33025200f4d83cd
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-10-24 13:18:07 +0800

    chore(install scripts): better name

---
+++ install_scripts/pacman_install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo pacman -S make openssl
+sudo pacman -S freetype2 libjpeg-turbo librsvg libtiff libwebp autoconf automake libtool gtk-doc
+sudo pacman -S cmake
+sudo pacman -S ImageMagick
+sudo pacman -S python-pip python-setuptools
