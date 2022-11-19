e681a111c46bb74c70797654a3fdc4a4155e4005
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-11-20 04:31:59 +0800

    chore(scripts): update pistol scripts

--- install_scripts/apps/pistol.sh
+++ install_scripts/apps/pistol.sh
@@ -6,8 +6,13 @@ set -o pipefail
 ######################################################################
 #                            Pistol Part                             #
 ######################################################################
+PISTOL_LINK="https://github.com/junegunn/fzf.git"
 if [[ -z $(command -v pistol) ]]; then
+    cd ~/tools
+    git clone "$PISTOL_LINK" --depth=1
+    cd ~/tools/pistol
     go install github.com/doronbehar/pistol/cmd/pistol@latest
+    cd ~/dotfiles/install_scripts/
 else
     printf "${tty_blue}Pistol${tty_reset} is already installed, skip it.\n"
 fi
