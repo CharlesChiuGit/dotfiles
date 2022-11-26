a8b54c3c318e584a365b8348a7e120160c71556f
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-11-26 12:04:05 +0800

    fix(pistol): more robust installation process

--- install_scripts/apps/pistol.sh
+++ install_scripts/apps/pistol.sh
@@ -6,13 +6,15 @@ set -o pipefail
 ######################################################################
 #                            Pistol Part                             #
 ######################################################################
+# INFO: needed packages: libmagic, file, file-devel
 PISTOL_LINK="https://github.com/doronbehar/pistol.git"
 if [[ -z $(command -v pistol) ]]; then
     echo "Install Pistol"
     cd ~/tools
     git clone "$PISTOL_LINK" --depth=1
     cd ~/tools/pistol
-    go install github.com/doronbehar/pistol/cmd/pistol@latest
+    make
+    make install
     cd ~/dotfiles/install_scripts/
 else
     printf "${tty_blue}Pistol${tty_reset} is already installed, skip it.\n"
