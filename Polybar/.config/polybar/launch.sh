1fc9b511705ebaf20dd575a12a05264f9c1daec3
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-12-01 12:26:46 +0800

    fix polybar on hlwm

--- Polybar/.config/polybar/launch.sh
+++ Polybar/.config/polybar/launch.sh
@@ -2,13 +2,33 @@
 
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
+# polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log
-polybar mainbar 2>&1 | tee -a /tmp/polybar1.log &
-disown
+# echo "---" | tee -a /tmp/polybar1.log
+# polybar mainbar 2>&1 | tee -a /tmp/polybar1.log &
+# disown
 
-echo "Polybar launched..."
+# echo "Polybar launched..."
+
+#First we allow ourselves to run herbstclient as hc
+hc() { "${herbstclient_command[@]:-herbstclient}" "$@"; }
+#Next I am saying that $monitor represents all my monitors
+monitor=${1:-0}
+# Terminate already running bar instances
+killall -q polybar
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Launch bars
+#polybar bottom &
+#polybar main &
+polybar top &
+# polybar bar2 &
+
+echo "Bars launched..."
+#this line of code will pad EACH monitor 32 pixels
+hc pad "$monitor" 32 0 0 0
+#this line of code will pad monitor 0 32 pixels
+#hc pad 0 32 0 0 0
