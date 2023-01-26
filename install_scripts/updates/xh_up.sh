ac7e2a7e2c2aa75772946848677b45a7e455aa33
CharlesChiuGit <charleschiu2012@gmail.comn>
2023-01-26 23:53:27 +0800

    typo

--- install_scripts/updates/xh_up.sh
+++ install_scripts/updates/xh_up.sh
@@ -11,14 +11,14 @@ remote_version=$(get_latest_release "ducaale/xh")
 remote_version_c=$(echo "$remote_version" | sed -E 's/v([0-9]+.[0-9]+.[0-9]+)/\1/')
 echo "Remote version: ${remote_version_c}"
 
-local_version=$(viu --version | awk '{print $2}')
+local_version=$(xh --version | awk '{print $2}')
 echo "Local version: ${local_version}"
 
 status=$(is_latest "$remote_version_c" "$local_version")
 if [ "$status" = false ]; then
-    printf "Update ${tty_blue}Xh...${tty_reset}\n"
-    cargo install xh
-    printf "${tty_blue}Xh${tty_reset} updated to ${tty_green}${remote_version}${tty_reset}.\n"
+	printf "Update ${tty_blue}Xh...${tty_reset}\n"
+	cargo install xh
+	printf "${tty_blue}Xh${tty_reset} updated to ${tty_green}${remote_version}${tty_reset}.\n"
 else
-    printf "${tty_blue}Xh${tty_reset} is up-to-date.\n"
+	printf "${tty_blue}Xh${tty_reset} is up-to-date.\n"
 fi
