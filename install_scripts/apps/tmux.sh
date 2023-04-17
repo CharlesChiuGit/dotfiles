84eacc2ac097918f8ea72b64856754f7062424ad
Charles Chiu <charleschiu2012@gmail.com>
2023-04-17 17:23:55 +0800

    chore(tmux): add dependency note.

--- install_scripts/apps/tmux.sh
+++ install_scripts/apps/tmux.sh
@@ -6,7 +6,7 @@ set -o pipefail
 ######################################################################
 #                             Tmux Part                              #
 ######################################################################
-# NOTE: sudo apt install libevent-dev
+# Ubuntu: sudo apt install cmake libevent-dev -y
 TMUX_DIR=$HOME/tools/tmux
 TMUX_SRC_NAME=$HOME/packages/tmux.tar.gz
 TMUX_LINK="https://github.com/tmux/tmux/releases/download/3.3a/tmux-3.3a.tar.gz"
