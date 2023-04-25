6a15fb2a266435d2c06e7e038c600e29fca9bbca
CharlesChiuGit <charleschiu@hey.com>
2023-04-25 16:48:58 +0800

    cleanup some rofi shit

---
+++ Rofi/.config/rofi/scripts/power-menu.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+chosen=$(printf "  Power Off\n  Restart\n  Lock\n  Sign Out" | rofi -dmenu -i -theme-str '@import "simple-tokyonight.rasi"')
+
+case "$chosen" in
+"  Power Off") systemctl poweroff ;;
+"  Restart") systemctl reboot ;;
+"  Lock") i3lock-fancy ;;
+"  Sign Out") bspc quit ;;
+
+*) exit 1 ;;
+esac
