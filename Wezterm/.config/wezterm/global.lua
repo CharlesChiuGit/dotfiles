76e5b9ad52e401b02b8d568809ddae3b6e022151
CharlesChiuGit <charleschiu@hey.com>
2023-04-28 17:27:28 +0800

    perf(wezterm): better way to get OS

--- Wezterm/.config/wezterm/global.lua
+++ Wezterm/.config/wezterm/global.lua
@@ -1,16 +1,29 @@
 local M = {}
 local wezterm = require("wezterm")
-local os_name = os.getenv("OS")
+
+local function getOS()
+	local osname = ""
+	-- Unix, Linux variants
+	local fh, err = assert(io.popen("uname -o 2>/dev/null", "r"))
+	if fh then
+		osname = fh:read()
+	end
+	return osname or os.getenv("OS")
+end
+
+local os_name = getOS()
 
 function M:load_variables()
 	self.is_windows = os_name == "Windows_NT"
 	self.is_macos = os_name == "Darwin"
-	self.is_linux = os_name == "Linux"
-	local win_home = self.is_windows and os.getenv("USERPROFILE") or wezterm.home_dir
+	self.is_linux = os_name == "GNU/Linux"
+	local home = wezterm.home_dir
 	if self.is_windows then
-		win_home = string.gsub(win_home, "\\", "/")
+		home = string.gsub(home, "\\", "/")
+	elseif self.is_linux then
+		home = home:sub(1)
 	end
-	self.home = self.is_windows and win_home or os.getenv("HOME")
+	self.home = home
 end
 
 M:load_variables()
