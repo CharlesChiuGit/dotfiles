9f82f4b03684e54738571a4a6ae5e1dff92d3184
CharlesChiuGit <charleschiu@hey.com>
2023-04-30 17:12:58 +0800

    fix(install script): fix some minor bug

--- install_scripts/updates/navi_up.sh
+++ install_scripts/updates/navi_up.sh
@@ -17,9 +17,9 @@ echo "Local version: ${local_version}"
 
 status=$(is_latest "$remote_version_c" "$local_version")
 if [ "$status" = false ]; then
-    printf "Update ${tty_blue}Navi...${tty_reset}\n"
-    cargo install navi --locked
-    printf "${tty_blue}Navi${tty_reset} updated to ${tty_green}${remote_version}${tty_reset}.\n"
+	printf "Update ${tty_blue}Navi...${tty_reset}\n"
+	cargo install navi --force
+	printf "${tty_blue}Navi${tty_reset} updated to ${tty_green}${remote_version}${tty_reset}.\n"
 else
-    printf "${tty_blue}Navi${tty_reset} is up-to-date.\n"
+	printf "${tty_blue}Navi${tty_reset} is up-to-date.\n"
 fi
--- install_scripts/updates/pistol_up.sh
+++ install_scripts/updates/pistol_up.sh
@@ -15,12 +15,13 @@ git fetch
 status=$(check_git_update "")
 
 if [[ $status = true ]]; then
-    printf "Update ${tty_blue}Pistol${tty_reset}.\n"
-    cd ~/tools/pistol
-    git pull
-    go install github.com/doronbehar/pistol/cmd/pistol@latest
+	printf "Update ${tty_blue}Pistol${tty_reset}.\n"
+	cd ~/tools/pistol
+	git reset –hard
+	git pull
+	go install github.com/doronbehar/pistol/cmd/pistol@latest
 elif [[ $status = false ]]; then
-    printf "${tty_blue}Pistol${tty_reset} is up-to-date!\n"
+	printf "${tty_blue}Pistol${tty_reset} is up-to-date!\n"
 fi
 
 cd ~/dotfiles/install_scripts/
