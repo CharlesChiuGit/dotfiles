720581ce01c462683b72c132cfc54833ccd87fe6
CharlesChiuGit <charleschiu@hey.com>
2023-05-17 18:16:40 +0800

    chore(lf): add `lfcd` to shell

--- Bash/.bash/bashenv.bash
+++ Bash/.bash/bashenv.bash
@@ -72,6 +72,22 @@ help() (
 	"$@" --help 2>&1 | bathelp
 )
 
+# Change working dir in shell to last dir in lf on exit.
+lfcd() {
+	tmp="$(mktemp)"
+	# `command` is needed in case `lfcd` is aliased to `lf`
+	command lf -last-dir-path="$tmp" "$@"
+	if [ -f "$tmp" ]; then
+		dir="$(cat "$tmp")"
+		rm -f "$tmp"
+		if [ -d "$dir" ]; then
+			if [ "$dir" != "$(pwd)" ]; then
+				cd "$dir"
+			fi
+		fi
+	fi
+}
+
 ## neovim support
 export EDITOR='nvim'
 # export LC_ALL="en_US.UTF-8"
