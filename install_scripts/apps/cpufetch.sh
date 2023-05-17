9cfb904eaafcfd7af4c95bac6cde49184c8f9827
CharlesChiuGit <charleschiu@hey.com>
2023-05-17 10:22:05 +0800

    chore(cpufetch): bump version to v1.04

--- install_scripts/apps/cpufetch.sh
+++ install_scripts/apps/cpufetch.sh
@@ -7,23 +7,23 @@ set -o pipefail
 #                           Cpufetch Part                            #
 ######################################################################
 CPUFETCH_DIR=$HOME/tools/cpufetch
-CPUFETCH_LINK="https://github.com/Dr-Noob/cpufetch/releases/download/v1.03/cpufetch_x86-64_linux"
+CPUFETCH_LINK="https://github.com/Dr-Noob/cpufetch/releases/download/v1.04/cpufetch_x86-64_linux"
 if [[ -z $(command -v cpufetch) ]]; then
-    echo "Install cpufetch"
+	echo "Install cpufetch"
 
-    if [[ ! -d $CPUFETCH_DIR ]]; then
-        echo "Creating cpufetch directory under tools directory"
-        mkdir -p "$CPUFETCH_DIR"
-        echo "Download to $HOME/tools/cpufetch directory"
-        wget "$CPUFETCH_LINK" -O "$CPUFETCH_DIR/cpufetch"
-        chmod +x "$CPUFETCH_DIR/cpufetch"
-    fi
+	if [[ ! -d $CPUFETCH_DIR ]]; then
+		echo "Creating cpufetch directory under tools directory"
+		mkdir -p "$CPUFETCH_DIR"
+		echo "Download to $HOME/tools/cpufetch directory"
+		wget "$CPUFETCH_LINK" -O "$CPUFETCH_DIR/cpufetch"
+		chmod +x "$CPUFETCH_DIR/cpufetch"
+	fi
 
-    if [[ "$ADD_TO_SYSTEM_PATH" = true ]]; then
-        cat <<EOT >>"$RC_FILE"
+	if [[ "$ADD_TO_SYSTEM_PATH" = true ]]; then
+		cat <<EOT >>"$RC_FILE"
 export PATH="$PATH:$HOME/tools/cpufetch"
 EOT
-    fi
+	fi
 else
-    printf "${tty_blue}Cpufetch${tty_reset} is already installed, skip it.\n"
+	printf "${tty_blue}Cpufetch${tty_reset} is already installed, skip it.\n"
 fi
