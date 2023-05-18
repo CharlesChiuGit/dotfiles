a9e9a56c3c924412db5be7fb899d0ef7645a8659
CharlesChiuGit <charleschiu@hey.com>
2023-05-18 16:26:10 +0800

    chore(ubuntu-pkg): remove unused pkgs

--- install_scripts/sys-apps/apt_install.sh
+++ install_scripts/sys-apps/apt_install.sh
@@ -5,26 +5,23 @@
 sudo apt install nala -y
 
 # Essential packages
-sudo nala install ssh vim htop git curl clang latexmk biber tree ninja-build \
-    make cmake build-essential software-properties-common mercurial binutils bison re2c \
-    python3-apt python3-pip python3-dev python3-setuptools python3.10-venv \
-    pandoc highlight gzip tar plocate pkg-config bzip2 openssl unzip \
-    libxml2-dev libevent-dev libbz2-dev libcurl4-openssl-dev libpng-dev libssl-dev \
-    libjpeg-dev libonig-dev libtidy-dev libxslt-dev libzip-dev libstdc++6 \
-    autoconf libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev \
-    libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev sqlite3 libsqlite3-dev \
-    libfreetype6-dev librsvg2-dev libtiff5-dev libwebp-dev libtool gtk-doc-tools \
-    libimage-exiftool-perl atool libmagic-dev \
-    libx11-dev libxmu-headers libxt-dev libxmu-dev -y
+sudo nala install ssh vim htop git curl clang biber tree ninja-build \
+	make cmake build-essential software-properties-common mercurial binutils bison re2c \
+	python3-apt python3-pip python3-dev python3-setuptools python3.10-venv \
+	pandoc highlight gzip tar plocate pkg-config bzip2 openssl unzip \
+	libxml2-dev libevent-dev libbz2-dev libcurl4-openssl-dev libpng-dev libssl-dev \
+	libjpeg-dev libonig-dev libtidy-dev libxslt1-dev libzip-dev libstdc++6 \
+	autoconf libyaml-dev libreadline-dev zlib1g-dev libncurses5-dev \
+	libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev sqlite3 libsqlite3-dev \
+	libfreetype6-dev librsvg2-dev libtiff5-dev libwebp-dev libtool gtk-doc-tools \
+	libimage-exiftool-perl atool libmagic-dev \
+	libx11-dev libxmu-headers libxt-dev libxmu-dev -y
 
 # Cool stuff
 sudo nala install neofetch hollywood cmatrix jp2a speedtest-cli toilet -y
 
 # Ranger utils support
 sudo nala install caca-utils ffmpeg librsvg2-bin atool unrar p7zip-full p7zip-rar -y
-
-# Snap install
-sudo snap install ascii-image-converter lolcat
 
 # system pip3 install
 sudo pip3 install pynvim gdown ntfy[emoji] asciinema
@@ -40,12 +37,6 @@ sudo nala install libpcre2-dev libz-dev libbz2-dev liblzma-dev liblz4-dev libzst
 
 # lldb: c/cpp/rust debugger
 sudo nala install lldb -y
-
-# Latex packages
-sudo nala install latexmk texlive-fonts-extra texlive-fonts-recommended texlive-latex-extra --no-install-recommends -y
-
-# sshfs
-sudo nala install sshfs -y
 
 # silicon
 sudo nala install expat -y
