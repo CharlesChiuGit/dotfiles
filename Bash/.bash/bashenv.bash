8287e67fb7479a344c830aa480074e4d9cb70701
CharlesChiuGit <charleschiu@hey.com>
2023-06-01 15:29:03 +0800

    fix(cider2): better path

--- Bash/.bash/bashenv.bash
+++ Bash/.bash/bashenv.bash
@@ -58,7 +58,7 @@ export PATH="$PATH:$HOME/tools/wezterm/usr/bin"
 export PISTOL_CHROMA_FORMATTER=terminal16m
 export PISTOL_CHROMA_STYLE=monokai
 export PATH="$PATH:$HOME/tools/logiops/build"
-export PATH="$PATH:$HOME/tools/cider2/opt"
+export PATH="$PATH:$HOME/tools/cider2/opt/cider2"
 
 ## Batcat for man & help
 _distro=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }' | tr -d '"')
