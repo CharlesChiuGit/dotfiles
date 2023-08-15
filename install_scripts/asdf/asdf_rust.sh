3c6d5894fb578b5909336725df5f3e7d08bf91f4
Charles Chiu <charleschiu2012@gmail.com>
2023-08-15 14:28:40 +0800

    chore(asdf-rust): bump version

--- install_scripts/asdf/asdf_rust.sh
+++ install_scripts/asdf/asdf_rust.sh
@@ -5,5 +5,5 @@ set -e
 #                         asdf Rust Install                          #
 ######################################################################
 asdf plugin-add rust https://github.com/asdf-community/asdf-rust.git
-asdf install rust 1.70.0
-asdf global rust 1.70.0
+asdf install rust 1.71.1
+asdf global rust 1.71.1
