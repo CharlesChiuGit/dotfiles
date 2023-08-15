0562b2558fd66d6d78b2d845237704248d79bcf6
Charles Chiu <charleschiu2012@gmail.com>
2023-08-15 14:26:59 +0800

    chore(asdf-golang): update version

--- install_scripts/asdf/asdf_golang.sh
+++ install_scripts/asdf/asdf_golang.sh
@@ -5,6 +5,6 @@ set -e
 #                        asdf Golang Install                         #
 ######################################################################
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-asdf install golang 1.20.5
-asdf global golang 1.20.5
+asdf install golang 1.20.7
+asdf global golang 1.20.7
 asdf reshim golang
