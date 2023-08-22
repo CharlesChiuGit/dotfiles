085d2fd6a757d7ca4b52c36e39c71978fdaa5bd5
Charles Chiu <charleschiu2012@gmail.com>
2023-08-22 18:16:00 +0800

    Create brew_install.sh

---
+++ scripts/macos/brew_install.sh
@@ -0,0 +1,37 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install --cask raycast
+brew install --cask cron
+brew install --cask arc
+brew install --cask hey
+brew install --cask caprine
+brew install --cask discord
+brew install --cask logseq
+brew install --cask obsidian
+brew install --cask logi-options-plus
+brew install --cask caprine
+brew install --cask cider
+brew install —cask appcleaner
+brew install --cask macupdater
+brew install --cask cheatsheet
+brew install --cask iina
+brew install --cask raindropio
+brew install --cask steam
+
+brew install wget
+brew install git
+brew install lazygit
+brew install git-delta
+brew install neovim
+brew install curl
+brew install starship
+brew install exa
+brew install zoxide
+brew install btop
+brew install fzf
+brew tap homebrew/cask-fonts
+brew install --cask font-jetbrains-mono-nerd-font
+
+
+https://github.com/th-ch/youtube-music
+logi option plus
