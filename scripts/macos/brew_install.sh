f7d9d7f51734ba3d18ba102222c1b7601dfe8c14
CharlesChiuGit <charleschiu@hey.com>
2023-08-31 17:48:49 +0800

    feat(macos): update install script

--- scripts/macos/brew_install.sh
+++ scripts/macos/brew_install.sh
@@ -1,37 +1,74 @@
+#!/bin/sh
+
+# Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Install cli-tools
+brew install wget
+brew install curl
+brew install git
+brew install sheldon
+brew install starship
+brew install exa
+brew install fzf
+brew install bat
+brew install zoxide
+brew tap homebrew/cask-fonts
+brew install --cask font-jetbrains-mono-nerd-font
+brew install joshuto
+brew install lazydocker
+brew install lazygit
+brew install git-delta
+brew install dua-cli
+brew install fzy
+brew install ripgrep
+brew install sd
+brew install xh
+brew install xq
+brew install neovim
+brew install btop
+brew install zellij
+brew install jdx/tap/rtx
+
+# Install gui-tools
+brew install brew-cask-completion
 brew install --cask raycast
 brew install --cask cron
 brew install --cask arc
 brew install --cask hey
-brew install --cask caprine
 brew install --cask discord
 brew install --cask logseq
 brew install --cask obsidian
 brew install --cask logi-options-plus
-brew install --cask caprine
-brew install --cask cider
-brew install —cask appcleaner
+brew install —-cask appcleaner
 brew install --cask macupdater
 brew install --cask cheatsheet
-brew install --cask iina
 brew install --cask raindropio
 brew install --cask steam
+brew install --cask cider
+brew install --cask iina
+brew install --cask wezterm
 
-brew install wget
-brew install git
-brew install lazygit
-brew install git-delta
-brew install neovim
-brew install curl
-brew install starship
-brew install exa
-brew install zoxide
-brew install btop
-brew install fzf
-brew tap homebrew/cask-fonts
-brew install --cask font-jetbrains-mono-nerd-font
+# Install langs
+rtx install node@16.16.0
+rtx install python@3.11.5
+brew install libyaml && rtx install ruby@3.2.2
+rtx install go@1.20.7
+rtx install java@openjdk-21
+rtx install rust@1.72.0
+rtx reshim
 
+# Install extras
+# th-ch/youtube-music
+wget -P ~/Downloads/ https://github.com/th-ch/youtube-music/releases/download/v1.20.0/YouTube-Music-1.20.0-arm64.dmg
+# NOTE: `xattr -cr /Applications/YouTube\ Music.app`
 
-https://github.com/th-ch/youtube-music
-logi option plus
+# symlink configs
+cargo install dt-cli
+dt-cli -c "~/Workspace/dotfiles/cli-utils/dt/macos.toml"
+bat cache --build
+
+# Post macOS setup
+## show all hidden files in Finder
+defaults write com.apple.finder AppleShowAllFiles TRUE
+killall Finder
