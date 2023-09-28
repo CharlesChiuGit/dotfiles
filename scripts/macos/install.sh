d01801f04514f29a5c83fb0155369368f8b79dad
CharlesChiuGit <charleschiu@hey.com>
2023-09-28 14:29:50 +0800

    perf(macos_install): update install script

--- scripts/macos/install.sh
+++ scripts/macos/install.sh
@@ -8,7 +8,7 @@ if [[ -z $(command -v brew) ]]; then
 	echo "Install Homebrew"
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-	printf "${tty_blue}Homebrew${tty_reset} is already installed, skip it.\n"
+	printf 'Homebrew is already installed, skip it.\n'
 fi
 
 # Add extra repo
@@ -16,7 +16,7 @@ repos=(
 	"homebrew/cask-fonts"
 )
 for i in "${repos[@]}"; do
-	brew tap $i
+	brew tap "$i"
 done
 
 # Install cli-tools
@@ -39,25 +39,27 @@ cli_tools=(
 	"libmagic"
 	"libyaml"
 	"lnav"
+	"lsd"
 	"neovim"
+	"openssl"
 	"poppler"
 	"ripgrep"
 	"ripsecrets"
 	"sd"
+	"sqlite"
+	"stats"
 	"tokei"
 	"topgrade"
+	"tree-sitter"
 	"wget"
 	"xdg-ninja"
 	"xh"
 	"xq"
 	"zellij"
 	"zoxide"
-	"lsd"
-	"gawk"
-	"grep"
 )
 for i in "${cli_tools[@]}"; do
-	brew install $i
+	brew install "$i"
 done
 
 # Install gui-tools
@@ -67,55 +69,60 @@ gui_tools=(
 	"cider"
 	"cron"
 	"discord"
+	"dropzone"
 	"font-jetbrains-mono-nerd-font"
 	"hey"
+	"hiddenbar"
 	"iina"
 	"logi-options-plus"
 	"logseq"
+	"maccy"
 	"obsidian"
 	"raindropio"
 	"raycast"
 	"steam"
-	"wezterm"
 	"visual-studio-code"
+	"wezterm"
 )
 for i in "${gui_tools[@]}"; do
-	brew install --cask $i
+	brew install --cask "$i"
 done
 
-# Instal zap-zsh
-zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
-
 # Install langs
-rtx plugin install rust https://github.com/code-lever/asdf-rust
 langs=(
 	"node@20.7.0"
 	"python@3.11.5"
 	"ruby@3.2.2"
 	"go@1.21.1"
 	"java@openjdk-21"
-	"rust@1.72.1"
 )
 for i in "${langs[@]}"; do
-	rtx install $i
+	rtx install "$i"
 done
 rtx reshim
 
 # cargo install
+cargo_pkg=(
+	"cargo-update"
+	"cargo-cache"
+	"dt-cli"
+)
+for i in "${cargo_pkg[@]}"; do
+	cargo install "$i" -j 4 --locked
+done
+
 cargo_repo=(
-	"https://github.com/nabijaczleweli/cargo-update.git"
-	"https://github.com/matthiaskrgr/cargo-cache.git cargo-cache"
-	"https://github.com/kamiyaa/joshuto.git"
-	"https://github.com/blurgyy/dt.git dt-cli"
 	"https://github.com/mmacedoeu/termpix.git"
+	"https://github.com/kamiyaa/joshuto.git"
 )
 for i in "${cargo_repo[@]}"; do
-	cargo install --git $i --locked
+	cargo install --git "$i" -j 4 --force --locked
 done
 
 # go install
-export HOMEBREW_PREFIX=$(brew --prefix)
-CGO_CFLAGS="-I${HOMEBREW_PREFIX}/include" CGO_LDFLAGS="-L${HOMEBREW_PREFIX}/lib" go install github.com/doronbehar/pistol/cmd/pistol@latest
+_homebrew_prefix=$(brew --prefix)
+export HOMEBREW_PREFIX=_homebrew_prefix
+# CGO_CFLAGS="-I${HOMEBREW_PREFIX}/include" CGO_LDFLAGS="-L${HOMEBREW_PREFIX}/lib" go install github.com/doronbehar/pistol/cmd/pistol@latest
 
 # Install extras
 if ! (find -x /Applications -type d -name YouTube\ Music.app) &>/dev/null; then
---
+++ scripts/macos/zsh_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# place zap-zsh config
+if [[ -f ~/.zshenv ]]; then
+	echo "Backup ~/.zshenv to ~/.zshenv.bak"
+	mv ~/.zshenv ~/.zshenv.bak
+fi
+cp ~/dotfiles/core-utils/zsh/.zshenv ~
+cp -r ~/dotfiles/core-utils/zsh/zsh ~/.config/
+
+# make sure zsh is default shell
+chsh -s "$(which zsh)"
+
+# Instal zap-zsh
+if [[ -z $(command -v zap) ]]; then
+	echo "Install zap-zsh"
+	zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+else
+	printf 'zap-zsh is already installed, skip it.\n'
+fi
