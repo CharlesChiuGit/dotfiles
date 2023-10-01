2fb6d2335f2ebc4536903260dd993af54c411481
CharlesChiuGit <charleschiu@hey.com>
2023-10-01 23:52:44 +0800

    fix(win_install): correct way to call env var

--- scripts/windows/link_config.ps1
+++ scripts/windows/link_config.ps1
@@ -1,39 +1,39 @@
 $dict = @{
     gitconfig = @(
-        "%USERPROFILE%\.gitconfig", 
-        "%USERPROFILE%\dotfiles\core-utils\git\.gitconfig"
+        "$Env:%USERPROFILE%\.gitconfig", 
+        "$Env:%USERPROFILE%\dotfiles\core-utils\git\.gitconfig"
     )
     pwsh_profile = @(
-        "%USERPROFILE%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1", 
-        "%USERPROFILE%\dotfiles\core-utils\pwsh\Microsoft.PowerShell_profile.ps1"
+        "$Env:%USERPROFILE%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1", 
+        "$Env:%USERPROFILE%\dotfiles\core-utils\pwsh\Microsoft.PowerShell_profile.ps1"
     )
     pwsh_config = @(
-        "%USERPROFILE%\Documents\PowerShell\powershell.config.json", 
-        "%USERPROFILE%\dotfiles\core-utils\pwsh\powershell.config.json"
+        "$Env:%USERPROFILE%\Documents\PowerShell\powershell.config.json", 
+        "$Env:%USERPROFILE%\dotfiles\core-utils\pwsh\powershell.config.json"
     )
     windows_terminal = @(
-        "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json", 
-        "%USERPROFILE%\dotfiles\windows_terminal\settings.json"
+        "$Env:%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json", 
+        "$Env:%USERPROFILE%\dotfiles\windows_terminal\settings.json"
     )
     lazygit = @(
-        "%APPDATA%\lazygit\config.yml", 
-        "%USERPROFILE%\dotfiles\cli-utils\lazygit\config.yml"
+        "$Env:%APPDATA%\lazygit\config.yml", 
+        "$Env:%USERPROFILE%\dotfiles\cli-utils\lazygit\config.yml"
     )
     lepton = @(
-        "%USERPROFILE%\.leptonrc", 
-        "%USERPROFILE%\dotfiles\gui-utils\lepton\.leptonrc"
+        "$Env:%USERPROFILE%\.leptonrc", 
+        "$Env:%USERPROFILE%\dotfiles\gui-utils\lepton\.leptonrc"
     )
     logseq = @(
-        "%USERPROFILE%\.logseq", 
-        "%USERPROFILE%\dotfiles\gui-utils\logseq\.logseq"
+        "$Env:%USERPROFILE%\.logseq", 
+        "$Env:%USERPROFILE%\dotfiles\gui-utils\logseq\.logseq"
     )
     starship = @(
-        "%USERPROFILE%\.config\starship.toml", 
-        "%USERPROFILE%\dotfiles\cli-utils\starship\starship.toml"
+        "$Env:%USERPROFILE%\.config\starship.toml", 
+        "$Env:%USERPROFILE%\dotfiles\cli-utils\starship\starship.toml"
     )
 }
 
-mkdir %APPDATA%\lazygit
+mkdir $Env:%APPDATA%\lazygit
 foreach ($key in $dict.Keys) {
     New-Item -ItemType SymbolicLink -Path $dict[$key][0] -Target $dict[$key][1]
 }
